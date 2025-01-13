26ff24fb9c07c5ac081db9077439452fffdcb667
satoru-naruko <naruko505@gmail.com>
2025-01-14 00:22:46 +0900

    UdpToTcpForwarderLog.hにデバッグログのプレフィックスを追加し、ログ出力を改善

--- UdpToTcpForwarderLog.h
+++ UdpToTcpForwarderLog.h
@@ -5,10 +5,12 @@
 
 #include <cstdio>
 
+#define DBGPREFIX "UDPTOTCPFORWARDER:"
+
 #define ENABLE_UDP_TO_TCP_FORWARDER_LOG
 
 #ifdef ENABLE_UDP_TO_TCP_FORWARDER_LOG
-#define UDP_TO_TCP_FORWARDER_LOG(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#define UDP_TO_TCP_FORWARDER_LOG(fmt, ...) printf("[%s:%d]" DBGPREFIX fmt, __FILE__, __LINE__, ##__VA_ARGS__)
 #else
 #define UDP_TO_TCP_FORWARDER_LOG(fmt, ...) // No logging
 #endif
